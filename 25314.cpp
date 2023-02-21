3bb8e7e4afa848d22cee57a385a4ec1e03bd64f2
jongchan0109 <123948591+jongchan0109@users.noreply.github.com>
2023-02-21 20:33:09 +0900

    Add files via upload

---
+++ 25314.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;cin>>n;
+    n/=4;
+    for(int i=1;i<=n;i++)
+        cout<<"long"<<" ";
+    cout<<"int";    
+    return 0;
+}
